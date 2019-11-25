d96b808afa7ff8466808d8596759da87b590b4ff
mr.mazurmaciej@gmail.com <mr.mazurmaciej@gmail.com>
2019-11-25 16:24:35 +0100

    Created HTML structure app with BEM names of classes

---
+++ script.js
@@ -0,0 +1,22 @@
+//objects
+const results = {
+  numberOfGames: 0,
+  numberOfWins: 0,
+  numberOfLosers: 0,
+  numberOfDraws: 0,
+}
+
+
+
+//variables
+const button = document.querySelector('button')
+
+console.log(button)
+
+/*
+1. Zmienne zebrac wszystkie
+2.
+
+
+
+*/
