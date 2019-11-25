c4e3f4fc564a879ad4bd898fec87fd717af6876c
mr.mazurmaciej@gmail.com <mr.mazurmaciej@gmail.com>
2019-11-25 21:37:27 +0100

    Simple logic of JS

--- script.js
+++ script.js
@@ -6,17 +6,80 @@ const results = {
   numberOfDraws: 0,
 }
 
-
+const choice = {
+  choiceAi: "",
+  choicePlayer: "",
+}
 
 //variables
-const button = document.querySelector('button')
+const chooseButton = document.querySelector('.user__choose-button');
+let choicePlayer = document.querySelector('.choice__player--result');
+let choiceAi = document.querySelector('.choice__ai--result');
+let choiceGameResult = document.querySelector('.choice__game-result--result');
 
-console.log(button)
+const playGame = (e) => {
+  gameChoiceAi()
+  choice.choicePlayer = e.toElement.alt
+  console.log(`PL: ${choice.choicePlayer}, AI: ${choice.choiceAi}`)
+  choicePlayer.textContent = `${choice.choicePlayer}`
+  choiceAi.textContent = `${choice.choiceAi}`
 
-/*
-1. Zmienne zebrac wszystkie
-2.
+
+}
+
+//AI choice
+const gameChoiceAi = () => {
+  const choices = ['paper', 'rock', 'scissors', 'spock', 'lizard'];
+  const choiceAi = choices[Math.floor(Math.random() * choices.length)];
+  choice.choiceAi = choiceAi;
+}
+
+//who win
+const whoWinGame = () => {
+
+  switch (choice.choicePlayer) {
+    case 'paper':
+      if (choice.choiceAi === 'spock' || choice.choiceAi === 'rock') { console.log('MACIEJ WYGRAL') }
+      else if (choice.choiceAi === 'paper') { console.log('REMIS') }
+      else if (choice.choiceAi === 'lizard' || choice.choiceAi === 'scissors') { console.log('AI WYGRAŁO') }
+      break;
+
+    case 'rock':
+      if (choice.choiceAi === 'lizard' || choice.choiceAi === 'scissors') { console.log('MACIEJ WYGRAL') }
+      else if (choice.choiceAi === 'rock') { console.log('REMIS') }
+      else if (choice.choiceAi === 'spock' || choice.choiceAi === 'paper') { console.log('AI WYGRAŁO') }
+      break;
+
+    case 'scissors':
+      if (choice.choiceAi === 'lizard' || choice.choiceAi === 'paper') { console.log('MACIEJ WYGRAL') }
+      else if (choice.choiceAi === 'scissors') { console.log('REMIS') }
+      else if (choice.choiceAi === 'spock' || choice.choiceAi === 'rock') { console.log('AI WYGRAŁO') }
+      break;
+
+    case 'spock':
+      if (choice.choiceAi === 'rock' || choice.choiceAi === 'scissors') { console.log('MACIEJ WYGRAL') }
+      else if (choice.choiceAi === 'spock') { console.log('REMIS') }
+      else if (choice.choiceAi === 'lizard' || choice.choiceAi === 'paper') { console.log('AI WYGRAŁO') }
+      break;
+
+    case 'lizard':
+      if (choice.choiceAi === 'spock' || choice.choiceAi === 'paper') { console.log('MACIEJ WYGRAL') }
+      else if (choice.choiceAi === 'lizard') { console.log('REMIS') }
+      else if (choice.choiceAi === 'scissors' || choice.choiceAi === 'rock') { console.log('AI WYGRAŁO') }
+      break;
+  }
+
+};
+
+//Data in results
+const numberOfGames = document.querySelector('.results__number-of-games--display').textContent = `${results.numberOfGames}`
+const numberOfWins = document.querySelector('.results__number-of-wins--display').textContent = `${results.numberOfWins}`
+const numberOfLosers = document.querySelector('.results__number-of-losers--display').textContent = `${results.numberOfLosers}`
+const numberOfDraws = document.querySelector('.results__number-of-draws--display').textContent = `${results.numberOfDraws}`
 
 
+//Add events listeners
+chooseButton.addEventListener('click', whoWinGame)
 
-*/
+document.querySelectorAll('.user-choice').forEach(block => block.addEventListener('click', playGame))
+
